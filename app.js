f92b45a4581333b2c8b050e416c20f4136162c37
Luis Nunes <dudupinto000@gmail.com>
2024-06-23 00:01:42 -0300

    Projeto Randomizador

---
+++ app.js
@@ -0,0 +1,56 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
+
+    if (quantidade > (ate - de + 1)) {
+        alert('A quantidade de números a serem sorteados não podem ser superiores ao intervalo de números escolhidos.');
+        return;
+    }
+
+   let sorteados = [];
+   let numero;
+
+   for (let i = 0; i < quantidade; i++) {
+    numero = gerarNumeroAleatorio(de, ate);
+
+    while (sorteados.includes(numero)) {
+        numero = gerarNumeroAleatorio(de, ate);
+    }
+
+    sorteados.push(numero);
+   }
+
+   let resultado = document.getElementById('resultado');
+   resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+   alterarStatusDoBotao();
+}
+
+function gerarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusDoBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado'); 
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`
+
+    alterarStatusDoBotao();
+}
